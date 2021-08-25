robertomartinez09515/native-mansion-screen-react

commit 95dc665eae270094860bb175925b669b707f2573

feat: expose transition progress (#890)
PR adding event with transition progress sent while animating between screens of native-stack. The progress is sent to both transitioning screens.

android/src/main/java/com/swmansion/rnscreens/ScreenFragment.kt
@@ -16,8 +16,11 @@ import com.facebook.react.uimanager.events.Event
 import com.swmansion.rnscreens.events.ScreenAppearEvent
 import com.swmansion.rnscreens.events.ScreenDisappearEvent
 import com.swmansion.rnscreens.events.ScreenDismissedEvent
+import com.swmansion.rnscreens.events.ScreenTransitionProgressEvent
 import com.swmansion.rnscreens.events.ScreenWillAppearEvent
 import com.swmansion.rnscreens.events.ScreenWillDisappearEvent
+import kotlin.math.max
+import kotlin.math.min
 
 open class ScreenFragment : Fragment {
     enum class ScreenLifecycleEvent {
@@ -29,6 +32,9 @@ open class ScreenFragment : Fragment {
     lateinit var screen: Screen
     private val mChildScreenContainers: MutableList<ScreenContainer<*>> = ArrayList()
     private var shouldUpdateOnResume = false
+    // if we don't set it, it will be 0.0f at the beginning so the progress will not be sent
+    // due to progress value being already 0.0f
+    private var mProgress = -1f
 
     constructor() {
         throw IllegalStateException(
@@ -118,33 +124,43 @@ open class ScreenFragment : Fragment {
 
     fun dispatchOnWillAppear() {
         dispatchEvent(ScreenLifecycleEvent.WillAppear, this)
+
+        dispatchTransitionProgress(0.0f, false)
     }
 
     fun dispatchOnAppear() {
         dispatchEvent(ScreenLifecycleEvent.Appear, this)
+
+        dispatchTransitionProgress(1.0f, false)
     }
 
     protected fun dispatchOnWillDisappear() {
         dispatchEvent(ScreenLifecycleEvent.WillDisappear, this)
+
+        dispatchTransitionProgress(0.0f, true)
     }
 
     protected fun dispatchOnDisappear() {
         dispatchEvent(ScreenLifecycleEvent.Disappear, this)
+
+        dispatchTransitionProgress(1.0f, true)
     }
 
     private fun dispatchEvent(event: ScreenLifecycleEvent, fragment: ScreenFragment) {
-        fragment.screen.let {
-            val lifecycleEvent: Event<*> = when (event) {
-                ScreenLifecycleEvent.WillAppear -> ScreenWillAppearEvent(it.id)
-                ScreenLifecycleEvent.Appear -> ScreenAppearEvent(it.id)
-                ScreenLifecycleEvent.WillDisappear -> ScreenWillDisappearEvent(it.id)
-                ScreenLifecycleEvent.Disappear -> ScreenDisappearEvent(it.id)
+        if (fragment is ScreenStackFragment) {
+            fragment.screen.let {
+                val lifecycleEvent: Event<*> = when (event) {
+                    ScreenLifecycleEvent.WillAppear -> ScreenWillAppearEvent(it.id)
+                    ScreenLifecycleEvent.Appear -> ScreenAppearEvent(it.id)
+                    ScreenLifecycleEvent.WillDisappear -> ScreenWillDisappearEvent(it.id)
+                    ScreenLifecycleEvent.Disappear -> ScreenDisappearEvent(it.id)
+                }
+                (it.context as ReactContext)
+                    .getNativeModule(UIManagerModule::class.java)
+                    ?.eventDispatcher
+                    ?.dispatchEvent(lifecycleEvent)
+                fragment.dispatchEventInChildContainers(event)
             }
-            (it.context as ReactContext)
-                .getNativeModule(UIManagerModule::class.java)
-                ?.eventDispatcher
-                ?.dispatchEvent(lifecycleEvent)
-            fragment.dispatchEventInChildContainers(event)
         }
     }
 
@@ -159,6 +175,31 @@ open class ScreenFragment : Fragment {
                         sc.topScreen?.fragment?.let { fragment -> dispatchEvent(event, fragment) }
                     }
                 }
+            }
+        }
+    }
+
+    fun dispatchTransitionProgress(alpha: Float, closing: Boolean) {
+        if (this is ScreenStackFragment) {
+            if (mProgress != alpha) {
+                mProgress = max(0.0f, min(1.0f, alpha))
+                /* We want value of 0 and 1 to be always dispatched so we base coalescing key on the progress:
+                 - progress is 0 -> key 1
+                 - progress is 1 -> key 2
+                 - progress is between 0 and 1 -> key 3
+             */
+                val coalescingKey = (if (mProgress == 0.0f) 1 else if (mProgress == 1.0f) 2 else 3).toShort()
+                val container: ScreenContainer<*>? = screen.container
+                check(container is ScreenStack) { "ScreenStackFragment added into a non-stack container" }
+                val goingForward = container.goingForward
+                (screen.context as ReactContext)
+                    .getNativeModule(UIManagerModule::class.java)
+                    ?.eventDispatcher
+                    ?.dispatchEvent(
+                        ScreenTransitionProgressEvent(
+                            screen.id, mProgress, closing, goingForward, coalescingKey
+                        )
+                    )
             }
         }
     }

.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   },
 
   settings: {
-    'import/core-modules': ['react-native-screens', 'react-native-screens/native-stack', 'react-native-screens/createNativeStackNavigator'],
+    'import/core-modules': ['react-native-screens', 'react-native-screens/native-stack', 'react-native-screens/createNativeStackNavigator', 'react-native-screens/reanimated'],
     'import/ignore': ['node_modules/react-native/index\\.js$'],
     'import/resolver': {
       node: {

TestsExample/App.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
+import {ReanimatedScreenProvider} from 'react-native-screens/reanimated';
+
 import Test42 from './src/Test42';
 import Test111 from './src/Test111';
 import Test263 from './src/Test263';
@@ -45,12 +47,14 @@ import Test860 from './src/Test860';
 import Test861 from './src/Test861';
 import Test865 from './src/Test865';
 import Test881 from './src/Test881';
+import Test887 from './src/Test887';
 import Test898 from './src/Test898';
 import Test913 from './src/Test913';
-import Test999 from './src/Test999';
-import Test1031 from './src/Test1031';
-import Test1032 from './src/Test1032';
 
 export default function App() {
-  return <Test42 />;
+  return (
+    <ReanimatedScreenProvider>
+      <Test42 />
+    </ReanimatedScreenProvider>
+  );
 }

TestsExample/metro.config.js
@@ -11,6 +11,7 @@ const modules = [
   '@react-navigation/native',
   'react-navigation',
   'react-navigation-stack',
+  'react-native-reanimated',
   'react-native-safe-area-context',
   ...Object.keys(pack.peerDependencies),
 ];

TestsExample/src/Test765.tsx
@@ -2,28 +2,31 @@ import * as React from 'react';
 import {
   Button,
   View,
+  Animated as RNAnimated,
 } from 'react-native';
 import {
   createAppContainer,
 } from 'react-navigation';
 
 import createNativeStackNavigator, {NativeStackNavigationOptions, NativeStackNavigationProp} from 'react-native-screens/createNativeStackNavigator';	
+import {useTransitionProgress} from 'react-native-screens';
+import {useReanimatedTransitionProgress} from 'react-native-screens/reanimated';
+import Animated, {useAnimatedStyle, useDerivedValue} from 'react-native-reanimated';
 
 const DEFAULT_STACK_OPTIONS : NativeStackNavigationOptions
  = {
-	headerBackTitleVisible: false,
 	headerTintColor: 'black',
 	headerTitleStyle: {
     fontFamily: 'arial',
   },
 	headerStyle: {
-		backgroundColor: 'white',
+		backgroundColor: 'powderblue',
 	},
 	cardStyle: {
 		backgroundColor: 'white',
 	},
+  headerTitle: 'Home',
 	hideShadow: true,
-	backButtonImage: undefined,
 	headerTopInsetEnabled: false,
 };
 
@@ -52,7 +55,6 @@ function makeStacks() {
 				screen: Home,
 				navigationOptions: {
 					cardTransparent: true,
-					gestureEnabled: false,
 					stackAnimation: 'fade',
 					cardStyle: {
 						backgroundColor: 'blue',
@@ -62,7 +64,6 @@ function makeStacks() {
 		},
 		{
 			headerMode: 'none',
-			mode: 'containedModal',
 		},
 	);
 	const MainStack = createNativeStackNavigator(
@@ -71,14 +72,12 @@ function makeStacks() {
 			Home5: {
 				screen: Home,
 				navigationOptions: {
-					gestureEnabled: false,
-					stackAnimation: 'fade',
+					stackAnimation: 'slide_from_right',
 				},
 			},
 		},
 		{
 			headerMode: 'none',
-			mode: 'containedModal',
 		},
 	);
 	return MainStack;
@@ -87,8 +86,27 @@ function makeStacks() {
 export default createAppContainer(makeStacks());
 
 function Home({navigation}: {navigation: NativeStackNavigationProp}) {
+  const reaProgress = useReanimatedTransitionProgress();
+  const sv = useDerivedValue(() => (reaProgress.progress.value < 0.5 ? (reaProgress.progress.value * 50) : ((1 - reaProgress.progress.value) * 50)) + 50);
+  const reaStyle = useAnimatedStyle(() => {
+    return {
+      width: sv.value,
+      height: sv.value,
+      backgroundColor: 'blue',
+    };
+  });
+
+  const {progress} = useTransitionProgress();
+  const opacity = progress.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1.0, 0.0 ,1.0],
+    extrapolate: 'clamp',
+  });
+
   return (
     <View style={{ flex: 1, backgroundColor: 'red' }}>
+      <RNAnimated.View style={{opacity, height: 50, backgroundColor: 'green'}} />
+      <Animated.View style={reaStyle} />
       <Button title="Go forward" onPress={() => navigation.navigate("Home5")} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>

TestsExample/src/Test887.tsx
@@ -1,0 +1,294 @@
+import * as React from 'react';
+import {
+  Animated as RNAnimated,
+  Button,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from 'react-native-screens/native-stack';
+import {useTransitionProgress} from 'react-native-screens';
+import {useReanimatedTransitionProgress} from 'react-native-screens/reanimated';
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+} from 'react-native-reanimated';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createNativeStackNavigator();
+
+const NestedStack = createStackNavigator();
+
+// Nested stack to check if transition progress values are passed properly through non native-stack navigators
+function NestedFirst() {
+  return (
+    <NestedStack.Navigator>
+      <NestedStack.Screen name="NestedFirst" component={First}/>
+    </NestedStack.Navigator>
+  )
+}
+
+type SimpleStackParams = {
+  First: undefined;
+  Second: undefined;
+  Third: undefined;
+};
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          stackAnimation: 'fade_from_bottom',
+          // stackPresentation: 'transparentModal',
+        }}>
+        <Stack.Screen name="First" component={NestedFirst} />
+        <Stack.Screen name="Second" component={Second} />
+        <Stack.Screen
+          name="Third"
+          component={Dialog}
+          options={{stackPresentation: 'modal'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// checking if many components can call useProgress at the same time
+function SiblingView() {
+  const reaProgress = useReanimatedTransitionProgress();
+  const sv = useDerivedValue(
+    () =>
+      (reaProgress.progress.value < 0.5
+        ? reaProgress.progress.value * 50
+        : (1 - reaProgress.progress.value) * 50) + 50,
+  );
+  const reaStyle = useAnimatedStyle(() => {
+    return {
+      width: sv.value,
+      height: sv.value,
+      backgroundColor: 'blue',
+    };
+  });
+
+  const {progress} = useTransitionProgress();
+
+  const opacity = progress.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1.0, 0.0, 1.0],
+    extrapolate: 'clamp',
+  });
+
+  return (
+     <>
+      <Animated.View style={reaStyle} />
+      <RNAnimated.View
+        style={{opacity, height: 50, width: '100%', backgroundColor: 'green'}}
+      />
+    </>
+  );
+}
+
+function First({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<SimpleStackParams, 'First'>;
+}) {
+  // using progress from both Animated and Reanimated context in the same Screen
+  const reaProgress = useReanimatedTransitionProgress();
+  const sv = useDerivedValue(
+    () =>
+      (reaProgress.progress.value < 0.5
+        ? reaProgress.progress.value * 50
+        : (1 - reaProgress.progress.value) * 50) + 50,
+  );
+  const reaStyle = useAnimatedStyle(() => {
+    return {
+      width: sv.value,
+      height: sv.value,
+      backgroundColor: 'blue',
+    };
+  });
+
+  const {progress} = useTransitionProgress();
+
+  const opacity = progress.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1.0, 0.0, 1.0],
+    extrapolate: 'clamp',
+  });
+
+  return (
+    <View style={{backgroundColor: 'red', flex: 1}}>
+      <Button
+        title="Tap me for second screen"
+        onPress={() => navigation.navigate('Second')}
+      />
+      <Button
+        title="Tap me for third screen"
+        onPress={() => navigation.push('Third')}
+      />
+      <Animated.View style={reaStyle} />
+      <SiblingView />
+      <SiblingView />
+      <RNAnimated.View
+        style={{opacity, height: 50, width: '100%', backgroundColor: 'green'}}
+      />
+    </View>
+  );
+}
+
+function Second({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<SimpleStackParams, 'Second'>;
+}) {
+  // using Animated.Value with the progress from context
+  const {progress} = useTransitionProgress();
+
+  const opacity = progress.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1.0, 0.0, 1.0],
+    extrapolate: 'clamp',
+  });
+
+  // use to check that nativeDriver works correctly
+  // React.useEffect(() => {
+  //   const inter = setInterval(() => {
+  //     let sum = 0;
+  //     for(let i = 0; i<1e8; i++) {
+  //       sum = sum + i;
+  //     }
+  //   }, 20);
+  //   return () => clearInterval(inter);
+  // })
+
+  return (
+    <View style={{backgroundColor: 'yellow', flex: 1}}>
+      <Button
+        title="Tap me for first screen"
+        onPress={() => navigation.navigate('First')}
+      />
+      <Button
+        title="Tap me for second screen"
+        onPress={() => navigation.push('Second')}
+      />
+      <Button
+        title="Tap me for third screen"
+        onPress={() => navigation.push('Third')}
+      />
+      <RNAnimated.View
+        style={{opacity, height: 50, backgroundColor: 'green'}}
+      />
+    </View>
+  );
+}
+
+const Dialog = ({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<SimpleStackParams, 'Third'>;
+}): JSX.Element => {
+  // using Animated with the progress
+  const {progress} = useTransitionProgress();
+  const reaProgress = useReanimatedTransitionProgress();
+  const sv = useDerivedValue(
+    () =>
+      (reaProgress.progress.value < 0.5
+        ? reaProgress.progress.value * 50
+        : (1 - reaProgress.progress.value) * 50) + 50,
+  );
+  const reaStyle = useAnimatedStyle(() => {
+    return {
+      width: sv.value,
+      height: sv.value,
+      backgroundColor: 'blue',
+    };
+  });
+
+  const val = progress.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0.1, 0.5, 0.1],
+    extrapolate: 'clamp',
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.heading}>Hey! Sign up for our newsletter!</Text>
+        <RNAnimated.View
+          style={{
+            width: 50,
+            height: 50,
+            opacity: val,
+            backgroundColor: 'blue',
+          }}
+        />
+        <Animated.View style={reaStyle} />
+        <TouchableOpacity
+          style={{...styles.button}}
+          onPress={() => navigation.goBack()}>
+          <Text style={styles.buttonText}>Please no.</Text>
+        </TouchableOpacity>
+        <Button
+          title="Tap me for third screen"
+          onPress={() => navigation.push('Third')}
+        />
+        <Button
+          title="Tap me for first screen"
+          onPress={() => navigation.navigate('First')}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000044',
+  },
+  wrapper: {
+    width: Dimensions.get('screen').width - 40,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5.0,
+    elevation: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    borderRadius: 8,
+  },
+  heading: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'dodgerblue',
+    height: 40,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});

android/src/main/java/com/swmansion/rnscreens/ScreenStack.kt
@@ -39,6 +39,7 @@ class ScreenStack(context: Context?) : ScreenContainer<ScreenStackFragment>(cont
     private var isDetachingCurrentScreen = false
     private var reverseLastTwoChildren = false
     private var previousChildrenCount = 0
+    var goingForward = false
     fun dismiss(screenFragment: ScreenStackFragment) {
         mDismissed.add(screenFragment)
         markUpdated()
@@ -173,6 +174,7 @@ class ScreenStack(context: Context?) : ScreenContainer<ScreenStackFragment>(cont
                 // We don't do it if the stack is nested since the parent will trigger these events in child
                 stackAnimation = StackAnimation.NONE
                 if (newTop.screen.stackAnimation !== StackAnimation.NONE && !isNested) {
+                    goingForward = true
                     newTop.dispatchOnWillAppear()
                     newTop.dispatchOnAppear()
                 }
@@ -222,6 +224,8 @@ class ScreenStack(context: Context?) : ScreenContainer<ScreenStackFragment>(cont
                 it.setTransition(transition)
             }
             // animation logic end
+            goingForward = shouldUseOpenAnimation
+
             if (shouldUseOpenAnimation &&
                 newTop != null && needsDrawReordering(newTop) &&
                 visibleBottom == null

android/src/main/java/com/swmansion/rnscreens/ScreenStackFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationSet
+import android.view.animation.Transformation
 import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -79,9 +80,9 @@ class ScreenStackFragment : ScreenFragment {
     }
 
     override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {
-        // this means that the fragment will appear without transition, in this case
-        // onViewAnimationStart and onViewAnimationEnd won't be called and we need to notify
-        // stack directly from here.
+        // this means that the fragment will appear with a custom transition, in the case
+        // of animation: 'none', onViewAnimationStart and onViewAnimationEnd
+        // won't be called and we need to notify stack directly from here.
         // When using the Toolbar back button this is called an extra time with transit = 0 but in
         // this case we don't want to notify. The way I found to detect is case is check isHidden.
         if (transit == 0 && !isHidden &&
@@ -192,7 +193,11 @@ class ScreenStackFragment : ScreenFragment {
             // there is already a listener for dismiss action added, which would be overridden
             // and also this is not necessary when going back since the lifecycle methods
             // are correctly dispatched then.
+            // We also add fakeAnimation to the set of animations, which sends the progress of animation
+            val fakeAnimation = ScreensAnimation(mFragment)
+            fakeAnimation.duration = animation.duration
             if (animation is AnimationSet && !mFragment.isRemoving) {
+                animation.addAnimation(fakeAnimation)
                 animation.setAnimationListener(mAnimationListener)
                 super.startAnimation(animation)
             } else {
@@ -201,6 +206,14 @@ class ScreenStackFragment : ScreenFragment {
                 set.setAnimationListener(mAnimationListener)
                 super.startAnimation(set)
             }
+        }
+    }
+
+    private class ScreensAnimation(private val mFragment: ScreenFragment) : Animation() {
+        override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+            super.applyTransformation(interpolatedTime, t)
+            // interpolated time should be the progress of the current transition
+            mFragment.dispatchTransitionProgress(interpolatedTime, !mFragment.isResumed)
         }
     }
 }

commit 1b614ffe0170fd0e05a419a5fe5fd2810e12e7e8

fix: send progress when container is null (#1074)
Fix a bug introduced in #890 where when container is already null, the app crashes.

android/src/main/java/com/swmansion/rnscreens/ScreenFragment.kt
@@ -190,8 +190,7 @@ open class ScreenFragment : Fragment {
              */
                 val coalescingKey = (if (mProgress == 0.0f) 1 else if (mProgress == 1.0f) 2 else 3).toShort()
                 val container: ScreenContainer<*>? = screen.container
-                check(container is ScreenStack) { "ScreenStackFragment added into a non-stack container" }
-                val goingForward = container.goingForward
+                val goingForward = if (container is ScreenStack) container.goingForward else false
                 (screen.context as ReactContext)
                     .getNativeModule(UIManagerModule::class.java)
                     ?.eventDispatcher